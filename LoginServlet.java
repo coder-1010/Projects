4d2d8678deb90fb7a2c934d9683160682feb995e
coder-1010 <60008502+coder-1010@users.noreply.github.com>
2023-07-10 10:04:21 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,34 @@
+package practiceProject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("name");
+		String password=request.getParameter("password");
+		if(password.equals("root") && name.equals("admin")) {
+			request.getRequestDispatcher("Dashboard.html").include(request, response);
+			out.print("Welcome "+name);
+			HttpSession session=request.getSession(); session.setAttribute("name",name);
+		}
+		else {
+			out.print("Sorry, username or password error!");
+			request.getRequestDispatcher("Login.html").include(request, response);
+		}
+		out.close();
+	}
+}
