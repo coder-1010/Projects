2d6f92a7ca03aa37ddcffe17e1f58f8b35142597
coder-1010 <60008502+coder-1010@users.noreply.github.com>
2023-07-10 16:12:42 +0530

    Update UsersController.java

--- PracticeProjects/DevOps/Authentication/UsersController.java
+++ PracticeProjects/DevOps/Authentication/UsersController.java
@@ -1,6 +1,3 @@
-package practiceProject.card3.three;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
